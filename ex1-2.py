13707c80345d7b15fcd4537f83432b879f62dd18
kwc1004 <kwc1004@yahoo.com>
2018-09-08 17:42:07 +0900

    jumin ******** using

--- ex1-2.py
+++ ex1-2.py
@@ -1,3 +1,13 @@
-# -*- coding: utf-8 -*-
-
-print("hello world")
+import re
+jumin = """
+kim chul-su 901212-1234567
+Lee jung-hee 761020-2312126
+Park chan-ho 980415-1123456
+"""
+p = re.compile(r'(?P<ju>\w+\s+\d{6}[-])\d{7}')
+m = p.sub('\g<ju>*******',jumin)
+if m:
+    print(m)
+else:
+    print("Not found")
+    
